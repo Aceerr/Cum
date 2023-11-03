6da27282e87d33fc37bb0dcffdb0a56e3dc443cb
Aceerr <christina162616@gmail.com>
2023-11-03 09:11:57 +0700

    Xu li dk cong nhanh master

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -38,6 +38,7 @@ namespace WinFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
+            if(txt1.Text != "")
             txt3.Text = (int.Parse(txt1.Text) + int.Parse(txt2.Text)).ToString();
         }
 
